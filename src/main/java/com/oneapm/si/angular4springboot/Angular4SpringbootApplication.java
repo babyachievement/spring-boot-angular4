61ff66acc418ade3689bb4a49748f8973b87eadb
haoqiang <haoqiang@oneapm.com>
2017-08-09 19:04:38 +0800

    init

---
+++ src/main/java/com/oneapm/si/angular4springboot/Angular4SpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.oneapm.si.angular4springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular4SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Angular4SpringbootApplication.class, args);
+    }
+}
